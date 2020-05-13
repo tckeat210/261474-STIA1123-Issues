68d1d6efe665465e5e20194942569361f18d4747
tckeat210 <60937264+tckeat210@users.noreply.github.com>
2020-05-13 13:12:54 +0800

    Update DKWat7.java

--- src/main/java/my/issues/issue10/DKWat7.java
+++ src/main/java/my/issues/issue10/DKWat7.java
@@ -1,4 +1,17 @@
 package my.issues.issue10;
 
-public class DKWat7 {
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Iterator;
+
+public class Array {
+  public static void displayStates(){
+   List<String> stateList = Arrays.asList("Pahang","Penang","Melaka","Johor","Kedah");
+   ListIterator<String> myListIterator = stateList.listIterator();
+      while (myListIterator.hasNext()){
+      System.out.println(myListIterator.next());
+      }
+   }
+
 }
